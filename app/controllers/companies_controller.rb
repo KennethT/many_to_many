92fbbdf8f78c018152ea36758fd2fc5abbfcd70d
Kenneth To <kennethkto@gmail.com>
2015-02-25 15:16:35 -0700

    add CRU of CRUD and show page to company views

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,4 +4,37 @@ class CompaniesController < ApplicationController
     @companies = Company.all
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company, notice: 'Company was succesfully created.'
+    else
+      render:new
+    end
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      redirect_to @company, notice: 'Company was successfully updated.'
+    else
+      render:edit
+    end
+  end
+
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :catch_phrase, :suffix)
+  end
+
 end
